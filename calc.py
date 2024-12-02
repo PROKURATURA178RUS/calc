5c30f6367d3856af8946dbe05caa6ad90f031d3f
PROKURATURA178RUS <priora05tonirovkas@tutamail.com>
2024-12-02 13:21:13 +0300

    123

--- calc.py
+++ calc.py
@@ -3,21 +3,17 @@ def add(a,b):
     c = (a + b)
     return c
 
-
 def mul(a,b):
-    c = (float(a)*float(b))
+    c = (a * b)
     return c
 
-
 def rem(a,b):
-        c = (float(a)-float(b))
+        c = (a - b)
         return c
-
 
 def div(a,b):
-        c = (float(a)/float(b))
+        c = (a / b)
         return c
-
 
 
 if '+' in num:
@@ -33,6 +29,18 @@ elif '*' in num:
     for i in range(1,len(list_numbers)):
         list_numbers[i] = float(list_numbers[i])      
         res = mul(res , list_numbers[i])
-print(res)
-
-
+    print(res)
+if '/' in num:
+    list_numbers = num.split('/')
+    res = float(list_numbers[0])
+    for i in range(1,len(list_numbers)):
+        list_numbers[i] = float(list_numbers[i])      
+        res = add(res , list_numbers[i])
+    print(res)
+if '-' in num:
+    list_numbers = num.split('-')
+    res = float(list_numbers[0])
+    for i in range(1,len(list_numbers)):
+        list_numbers[i] = float(list_numbers[i])      
+        res = add(res , list_numbers[i])
+    print(res)
